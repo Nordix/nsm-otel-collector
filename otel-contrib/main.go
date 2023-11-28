8b9e9634407a989344f555977edc7aa9cc9d470b
Gábor Barna <robaganrab@gmail.com>
2023-11-28 15:27:57 +0100

    Update to OTEL 0.89.0

--- otel-contrib/main.go
+++ otel-contrib/main.go
@@ -18,7 +18,7 @@ func main() {
 	info := component.BuildInfo{
 		Command:     "otelcol-contrib",
 		Description: "OpenTelemetry Collector Contrib",
-		Version:     "0.88.0",
+		Version:     "0.89.0",
 	}
 
 	if err := run(otelcol.CollectorSettings{BuildInfo: info, Factories: factories}); err != nil {
