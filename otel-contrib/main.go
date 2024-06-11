4efa47c2d1f32a1fb908a329548389d62c70c251
Botond Szirtes <botond.szirtes@est.tech>
2024-06-11 16:11:56 +0200

    Update to OTEL 0.101.0

--- otel-contrib/main.go
+++ otel-contrib/main.go
@@ -21,7 +21,7 @@ func main() {
 	info := component.BuildInfo{
 		Command:     "otelcol-contrib",
 		Description: "OpenTelemetry Collector Contrib",
-		Version:     "0.100.0",
+		Version:     "0.101.0",
 	}
 
 	set := otelcol.CollectorSettings{
